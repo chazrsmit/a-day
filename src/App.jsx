9052805e14c91326a94bf1fc6301b87adadec2b7
Charlotte <smit-charlotte@outlook.com>
2026-06-11 20:36:17 +0200

    useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Links from './components/Links'
 import ImageLayer from './components/ImageLayer'
